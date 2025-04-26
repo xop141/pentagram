18fa544f75c966c495e0a63e7375247484920c78
xop141 <lowsens400@gmail.com>
2025-04-26 20:43:34 +0800

    asd

--- OneDrive/Desktop/asdd/frontend/src/app/page.tsx
+++ OneDrive/Desktop/asdd/frontend/src/app/page.tsx
@@ -3,6 +3,7 @@ import Login from "./login/page"
 export default function FeedPage() {
   return (
     <div >
+      asd
       <Login/>
     </div>
   );
--- OneDrive/Desktop/asdd/frontend/src/utils/api.tsx
+++ OneDrive/Desktop/asdd/frontend/src/utils/api.tsx
@@ -1,4 +1,4 @@
-export const API = "http://localhost:4000"   
+export const API = "http://localhost:9000"   
 
 
 
